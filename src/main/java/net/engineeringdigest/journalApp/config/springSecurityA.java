1ed6e75427b2853e098d3ac6ea1ac7fa6998d7bc
SAURABH <saurabh.srivastava1024@gmail.com>
2025-09-05 14:34:25 +0530

    O-Auth intergration

---
+++ src/main/java/net/engineeringdigest/journalApp/config/springSecurityA.java
@@ -0,0 +1,6 @@
+package net.engineeringdigest.journalApp.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class springSecurityA extends WebSecurityConfigurerAdapter {
+}
